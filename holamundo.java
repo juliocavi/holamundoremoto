8b1d599eaf34699fca1925c07eaf37abaed7ba75
Julio Cachon Villadangos <juliocachonvi@gmail.com>
2017-09-29 12:46:23 +0200

    hola mundo, edad, localidad, mes

--- holamundo.java
+++ holamundo.java
@@ -5,5 +5,6 @@ public class holamundo{
 		System.out.println("Hola mundo!");
 		System.out.println("21");
 		System.out.println("santa maria del paramo");
+		System.out.println("septiembre");
 	}
 }
